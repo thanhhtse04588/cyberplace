52360e4e7348f74afaf2034fdac8eb7e11241881
thanhhtse04588 <41248151+thanhhtse04588@users.noreply.github.com>
2020-05-09 00:26:35 +0700

    build war prod

--- src/main/java/com/capstone/cyberplace/CyberplaceApplication.java
+++ src/main/java/com/capstone/cyberplace/CyberplaceApplication.java
@@ -2,12 +2,19 @@ package com.capstone.cyberplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CyberplaceApplication {
+public class CyberplaceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CyberplaceApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CyberplaceApplication.class);
+    }
 
 }
